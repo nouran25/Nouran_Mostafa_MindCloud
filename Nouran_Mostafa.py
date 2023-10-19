46cf491eb654890288a360d3772d57fdabb2f0ae
Nouran Mostafa <132231010+nouran25@users.noreply.github.com>
2023-10-19 20:37:08 +0300

    Create Nouran_Mostafa.py

---
+++ Nouran_Mostafa.py
@@ -0,0 +1,7 @@
+name = "Nouran Mostafa"
+age = 22
+department = "Electronics & Communication"
+graduation_year = 2025
+favorite_team = "Software"
+
+print("Name: {}\nAge: {}\nDepartment: {}\nGraduation Year: {}\nFavorite Team: {}".format(name, age, department, graduation_year, favorite_team))
